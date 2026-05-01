1c0dbd06a7488aacf6cc524cd79779ed63498bbc
unknown <usf.belhadj@gmail.com>
2026-05-01 20:48:07 +0100

    teszt

--- scripting_cyber/0x02-ps_scripting/5-hash.ps1
+++ scripting_cyber/0x02-ps_scripting/5-hash.ps1
@@ -6,18 +6,14 @@ $ErrorActionPreference = 'Stop'
 function Get-SHA256 {
     param ([string] $Input)
 
-    $bytes  = [System.Text.Encoding]::UTF8.GetBytes($Input)
-    $hash   = [System.Security.Cryptography.SHA256]::Create().ComputeHash($bytes)
-    $hex    = ($hash | ForEach-Object { $_.ToString("x2") }) -join ''
+    $bytes = [System.Text.Encoding]::UTF8.GetBytes($Input)
+    $hash  = [System.Security.Cryptography.SHA256]::Create().ComputeHash($bytes)
+    $hex   = ($hash | ForEach-Object { $_.ToString("x2") }) -join ''
 
     Write-Output "SHA256: $hex"
 }
 
 function Main {
-    if (-not $args[0]) {
-        Write-Output "Usage: pwsh 10-hash.ps1 <word>"
-        exit 1
-    }
     Get-SHA256 -Input $args[0]
 }
 
