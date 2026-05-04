fdaa1aa743516023479a154e892a5a1f438fc7be
unknown <usf.belhadj@gmail.com>
2026-05-04 12:02:30 +0100

    push

--- linux_security/network_protocols/4-audit.sh
+++ linux_security/network_protocols/4-audit.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-grep -hEv '^(#|$)' /etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf 2>/dev/null
+files=(/etc/ssh/sshd_config /etc/ssh/sshd_config.d/*.conf)
+
+grep -hEv '^(#|$)' "${files[@]}" 2>/dev/null
